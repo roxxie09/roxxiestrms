d8b82f6970f5ae99ec27b5405123b9a473f79f15
cooldudes9001 <Angel.petricek@proton.me>
2024-09-17 23:25:37 -0700

    f

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,25 +1,19 @@
-// Set the date we're counting down to
-var countDownDate = new Date("September 18, 2024 03:00:00 GMT-0800").getTime();
+const countdownElement = document.getElementById('countdown');
 
-// Update the countdown every 1 second
-var x = setInterval(function() {
-    // Get current date and time
-    var now = new Date().getTime();
+function updateCountdown() {
+    const eventDate = new Date('2024-09-17T23:26:00-08:00'); // Change this to your desired time
+    const now = new Date();
+    const timeDiff = eventDate - now;
 
-    // Calculate the distance between now and the countdown date
-    var distance = countDownDate - now;
-
-    // Time calculations for hours, minutes and seconds
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Display the result in the countdown element
-    document.getElementById("countdown").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
-
-    // If the countdown is over, display a message
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("countdown").innerHTML = "Event Started";
+    if (timeDiff > 0) {
+        const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+        
+        countdownElement.innerHTML = `${hours}h ${minutes}m ${seconds}s`;
+    } else {
+        countdownElement.innerHTML = "Event has started!";
     }
-}, 1000);
+}
+
+setInterval(updateCountdown, 1000);
