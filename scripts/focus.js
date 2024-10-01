b160e373e313a82faaf8af02fbc6634c2859d903
roxxie09 <Angel.petricek@proton.me>
2024-09-30 20:09:52 -0700

    ab

--- scripts/focus.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    var inputElement = document.getElementById('yourInputId'); // Replace with your input ID
-    if (inputElement) {
-        inputElement.focus();
-    }
-};
