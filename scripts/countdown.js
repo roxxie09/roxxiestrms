4f5768b207ed31c542153f5e30008e070ca94dd4
roxxie09 <Angel.petricek@proton.me>
2024-11-03 08:11:43 -0800

    Update countdown.js

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,13 +1,16 @@
-// After adding the new events
 function initializeCountdowns() {
     document.querySelectorAll('[data-time]').forEach(function(eventCard) {
+        // Parse the data-time and data-end-time strings into Date objects
         var countDownDate = new Date(eventCard.getAttribute('data-time')).getTime();
         var endTime = new Date(eventCard.getAttribute('data-end-time')).getTime();
         
+        // Get the timer element
         var timerElement = eventCard.querySelector('.countdown-timer');
 
         var intervalId = setInterval(function() {
-            var now = new Date().getTime();
+            var now = new Date().getTime(); // Current time
+
+            // Calculate the remaining time until the countdown date
             var distance = countDownDate - now;
 
             // Calculate days, hours, minutes, and seconds
@@ -16,8 +19,10 @@ function initializeCountdowns() {
             var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
             var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+            // Update the countdown display
             timerElement.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
+            // Check if the event has started
             if (distance < 0) {
                 clearInterval(intervalId);
                 timerElement.innerHTML = "LIVE";
@@ -33,6 +38,3 @@ function initializeCountdowns() {
         }, 1000);
     });
 }
-
-// Call this function after you add new events to the DOM
-initializeCountdowns();
