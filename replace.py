dd8ae53d4c6eab8ff6bb496f29fa2f7a6b4bf12f
turniptrainwrek <181801487+roxxie09@users.noreply.github.com>
2025-11-01 10:15:26 -0700

    f

--- replace.py
+++ replace.py
@@ -11,8 +11,8 @@ for filename in os.listdir(folder):
         with open(filepath, 'r', encoding='utf-8') as file:
             content = file.read()
 
-        # Replace all instances of "slux.009292.xyz" with "sb.009292.xyz"
-        updated_content = content.replace("slux.009292.xyz", "sb.009292.xyz")
+        # Replace all instances of "sb.009292.xyz" with "tri.qiyarmedia.com"
+        updated_content = content.replace("sb.009292.xyz", "tri.qiyarmedia.com")
 
         # Write the changes back to the file
         with open(filepath, 'w', encoding='utf-8') as file:
