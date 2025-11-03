24e169b6f46939b6e4adfff4035ee240c58f7f0a
turniptrainwrek <Angel.petricek@proton.me>
2025-11-02 17:15:44 -0800

    z

--- replace.py
+++ replace.py
@@ -11,8 +11,8 @@ for filename in os.listdir(folder):
         with open(filepath, 'r', encoding='utf-8') as file:
             content = file.read()
 
-        # Replace all instances of "roxiestreams.cc" with "roxiestreams.live"
-        updated_content = content.replace("roxiestreams.cc", "roxiestreams.live")
+        # Replace all instances of "roxiestreams.live" with "roxiestreams.cc"
+        updated_content = content.replace("roxiestreams.live", "roxiestreams.cc")
 
         # Write the changes back to the file
         with open(filepath, 'w', encoding='utf-8') as file:
